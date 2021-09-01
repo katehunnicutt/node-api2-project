f825b83f1b72fc7446b52d26759e2417b8a3fbeb
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-08-31 18:32:19 -0700

    getting there

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -24,12 +24,49 @@ router.get("/", (req, res) => {
 
 //get post by specific id
 router.get("/:id", (req, res) => {
-
+  Post.findById(req.params.id)
+  .then(post => {
+    if(post) {
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist"
+      })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: "The post information could not be retrieved",
+      err: err.message,
+      stack: err.stack,
+    })
+  })
 })
 
 //create new post
+//
 router.post("/", (req, res) => {
-
+  const {title, contents} = req.body 
+  if(!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post"
+    })
+  } else {
+    Post.insert({title, contents})
+    .then(({id}) => {
+      return Post.findById(id)
+    })
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(err => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+          err: err.message,
+          stack: err.stack
+        })
+    })
+  }
 })
 
 //edit post by id
